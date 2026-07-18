6c89783b23396a82e39858b64e8972eb84fd9453
javaAgentAI <3159979021@qq.com>
2026-07-18 20:11:52 +0800

    test: add unit test coverage for core modules (AgentService, DomainTools, KnowledgeBaseService)

---
+++ mediguide-ai/src/test/java/com/agentstack/mediguide/MediguideApplicationTests.java
@@ -0,0 +1,14 @@
+package com.agentstack.mediguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+@SpringBootTest
+@ActiveProfiles("test")
+class MediguideApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
